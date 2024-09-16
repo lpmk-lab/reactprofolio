dacb0c521382d9fa466e40580f32b41d3e55abe1
lapyaeminkhant.sbs <lapyaeminkhant.sbs@gmail.com>
2024-09-16 21:50:16 +0630

    merge

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -9,13 +9,21 @@ const Navbar = () => {
             {/* Logo Section */}
             <div className='flex flex-shrink-0 items-center'>
                 <img className="mx-2 w-10" src={logo} alt="logo" />
-            </div> 
+            </div>
             {/* Social Icons Section */}
             <div className="flex items-center justify-center gap-6 text-2xl lg:text-3xl">
-                <FaLinkedin className="hover:text-blue-600 transition duration-300" />
-                <FaGithub className="hover:text-gray-400 transition duration-300" />
-                <FaInstagram className="hover:text-pink-500 transition duration-300" />
-                <FaSquareTwitter className="hover:text-blue-400 transition duration-300" />
+                <a href="https://www.linkedin.com/in/la-pyae-min-khant-6410b12b4/" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin style={{ fontSize: '24px' }} className="hover:text-blue-600 transition duration-300" />
+                </a>
+                <a href="https://github.com/lpmk-lab" target="_blank" rel="noopener noreferrer">
+                    <FaGithub style={{ fontSize: '24px' }} className="hover:text-gray-400 transition duration-300" />
+                </a>
+                <a href="https://www.instagram.com/yourusername" target="_blank" rel="noopener noreferrer">
+                    <FaInstagram style={{ fontSize: '24px' }} className="hover:text-pink-500 transition duration-300" />
+                </a>
+                <a href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer">
+                    <FaSquareTwitter style={{ fontSize: '24px' }} className="hover:text-blue-400 transition duration-300" />
+                </a>
             </div>
         </nav>
     )
